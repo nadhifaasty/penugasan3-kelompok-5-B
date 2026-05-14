a12c6943e31ef807913c51c36e87e20e3c447530
Iim <syauqikp2@gmail.com>
2026-05-14 20:07:44 +0700

    perbaikan db karena laptop kena bongkar

--- app/main.py
+++ app/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
 from app.routers import role_router, user_router, account_router, registration_router, event_router
+from app.database import engine, Base 
+
+import app.models 
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="CRUD API", version="1.0.0")
 
+# Register Router
 app.include_router(role_router.router)
 app.include_router(user_router.router)
 app.include_router(account_router.router)
